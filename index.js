411fefd829280e7963a13be9fae94d97f5a269dc
arifmridhapranto <mridhapranto647@gmail.com>
2021-11-15 21:35:58 +0600

    done

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ async function run() {
 
     const database = client.db("Stedman_Wheel");
     const productsCollection = database.collection("Products");
-    // const UsersCollection = database.collection("Users");
-    // const ContactsCollection = database.collection("Contacts");
+    const UsersCollection = database.collection("Users");
+    const ContactsCollection = database.collection("Contacts");
     const OrdersCollection = database.collection("Orders");
-    // const ReviewCollection = database.collection("Review");
+    const ReviewCollection = database.collection("Review");
 
     //get Products from Database
     app.get("/products", async (req, res) => {
@@ -97,6 +97,70 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const cursor = await OrdersCollection.deleteOne(query);
       res.json(cursor);
+    });
+    //get all Reviews from the database
+    app.get("/review", async (req, res) => {
+      const query = {};
+      const cursor = await ReviewCollection.find(query).toArray();
+      res.json(cursor);
+    });
+    //Add users review into Database
+    app.post("/addreview", async (req, res) => {
+      const review = req.body;
+      const result = await ReviewCollection.insertOne(review);
+      console.log(result);
+      res.json(result);
+    });
+    // add contact form details to database
+    app.post("/contact", async (req, res) => {
+      const contactForm = req.body;
+      const result = await ContactsCollection.insertOne(contactForm);
+      console.log(result);
+      res.json(result);
+    });
+    //get user by email
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await UsersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "Admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    //add new user into Database
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await UsersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    // add or update user into Database
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await UsersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    // add a admin
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "Admin" } };
+      const result = await UsersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+      console.log(result);
     });
   } finally {
     // await client.close();
